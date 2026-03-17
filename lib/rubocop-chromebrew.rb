7890ce7e4f5cb17815d0d7deff292ce6792fb42b
Zopolis4 <creatorsmithmdt@gmail.com>
2026-03-17 13:40:32 +1100

    Convert to plugin system

--- lib/rubocop-chromebrew.rb
+++ lib/rubocop-chromebrew.rb
@@ -1,8 +1,6 @@
 require 'rubocop'
 
 require_relative 'rubocop/chromebrew'
-require_relative 'rubocop/chromebrew/inject'
-
-RuboCop::Chromebrew::Inject.defaults!
+require_relative 'rubocop/chromebrew/plugin'
 
 require_relative 'rubocop/cop/chromebrew_cops'
